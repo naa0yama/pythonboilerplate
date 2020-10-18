f4b236375be0e9a3ce9cf8cc0b7c0c119af590f7
Naoki Aoyama <9667078+naa0yama@users.noreply.github.com>
2020-10-18 14:50:03 +0000

    fix: tests

--- src/pythonboilerplate/func.py
+++ src/pythonboilerplate/func.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""python_boilerplate."""
+"""pythonboilerplate."""
 
 
 # %%
---
+++ tests/conftest.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""test top conftest."""
+
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + "/../"))
